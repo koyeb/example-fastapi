49c46a08c53bc3aadd2e352085ea694b24f8b8b8
Vibhu Bindal <vibhubindal2013@gmail.com>
2024-02-16 12:41:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/restaurent/{restaurent_id}")
+def read_item(restaurent_id: int, q: Union[str, None] = None):
+    return {"restaurent_id": restaurent_id, "q": q}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-    
+
