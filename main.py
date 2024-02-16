86ec2f931a3bdeeef648c539969c1054f4ed0edf
Vibhu Bindal <vibhubindal2013@gmail.com>
2024-02-16 12:34:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/restaurent/{restaurent_id}")
+def read_item(restaurent_id: int, q: Union[str, None] = None):
+    return {"restaurent_id": restaurent_id, "q": q}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
